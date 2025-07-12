77e519c93fb80ea1a311b6350fea319e02e71251
Sacha Senas <sachasenas@gmail.com>
2025-07-12 03:30:06 +0200

    oui

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -1,51 +1,31 @@
 return {
-  { -- Collection of various small independent plugins/modules
+  {
     'echasnovski/mini.nvim',
     config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+      local ai = require 'mini.ai'
+      ai.setup {
+        n_lines = 500,
+        custom_textobjects = {
+          -- Treesitter-powered textobjects
+          f = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }, {}),
+          c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }, {}),
+          p = ai.gen_spec.treesitter({ a = '@parameter.outer', i = '@parameter.inner' }, {}),
+          d = ai.gen_spec.treesitter({ a = '@conditional.outer', i = '@conditional.inner' }, {}),
+          l = ai.gen_spec.treesitter({ a = '@loop.outer', i = '@loop.inner' }, {}),
+          a = ai.gen_spec.treesitter({ a = '@assignment.outer', i = '@assignment.inner' }, {}),
+        },
+      }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return '%2l:%-2v'
       end
 
-      -- === ADDED mini.icons SETUP HERE ===
-      -- Icons for different file types and Git statuses
-      -- This is what Oil.nvim (and other plugins) will use for file icons
-      require('mini.icons').setup({
-        -- By default, mini.icons tries to use Nerd Fonts if available.
-        -- You can customize options here if needed, for example:
-        -- use_devicons = true, -- if you specifically want to use nvim-web-devicons glyphs
-        -- (though mini.icons has its own good defaults)
-      })
-      -- ===================================
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
+      require('mini.icons').setup()
     end,
   },
 }
--- vim: ts=2 sts=2 sw=2 et
--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -1,109 +1,76 @@
 return {
-  { -- Highlight, edit, and navigate code
+  {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    main = 'nvim-treesitter.configs',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' }, -- Added 'python' here
-      -- Autoinstall languages that are not installed
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'python',
+      },
       auto_install = true,
       highlight = {
         enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
 
-      -- === START: nvim-treesitter-textobjects configuration ===
-      -- This 'textobjects' table is where the magic happens for nvim-treesitter-textobjects
+      -- Disable conflicting textobject select module
       textobjects = {
         select = {
-          enable = true,
-          -- Automatically jump forward to textobj, similar to targets.vim
-          lookahead = true,
-          keymaps = {
-            -- Python-specific text objects:
-            -- See: https://github.com/nvim-treesitter/nvim-treesitter-textobjects/blob/master/queries/python/textobjects.scm
-            ['af'] = '@function.outer', -- Around a function definition
-            ['if'] = '@function.inner', -- Inside a function definition
-            ['ac'] = '@class.outer', -- Around a class definition
-            ['ic'] = '@class.inner', -- Inside a class definition
-            ['ap'] = '@parameter.outer', -- Around a parameter (e.g., in a function call/definition)
-            ['ip'] = '@parameter.inner', -- Inside a parameter
-
-            -- General text objects that work well with many languages
-            ['aa'] = '@parameter.outer', -- around an argument/parameter
-            ['ia'] = '@parameter.inner', -- inside an argument/parameter
-            ['ad'] = '@conditional.outer', -- around a conditional (if/else)
-            ['id'] = '@conditional.inner', -- inside a conditional
-            ['al'] = '@loop.outer', -- around a loop (for/while)
-            ['il'] = '@loop.inner', -- inside a loop
-            -- ['ab'] = '@block.outer', -- around a block of code (e.g., in Python: the indented body of if/for/func)
-            -- ['ib'] = '@block.inner', -- inside a block of code
-            ['a='] = '@assignment.outer', -- around an assignment
-            ['i='] = '@assignment.inner', -- inside an assignment
-          },
+          enable = false,
         },
         swap = {
           enable = true,
           swap_next = {
-            ['<leader>ap'] = '@parameter.inner', -- Swap with next parameter
+            ['<leader>ap'] = '@parameter.inner',
           },
           swap_previous = {
-            ['<leader>aP'] = '@parameter.inner', -- Swap with previous parameter
+            ['<leader>aP'] = '@parameter.inner',
           },
         },
         move = {
           enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
+          set_jumps = true,
           goto_next_start = {
-            [']m'] = '@function.outer', -- Next function start
-            [']]'] = '@class.outer', -- Next class start
-            [']i'] = '@conditional.outer', -- Next if/else start
-            [']l'] = '@loop.outer', -- Next loop start
+            [']m'] = '@function.outer',
+            [']]'] = '@class.outer',
+            [']i'] = '@conditional.outer',
+            [']l'] = '@loop.outer',
           },
           goto_next_end = {
-            [']M'] = '@function.outer', -- Next function end
-            [']['] = '@class.outer', -- Next class end
-            [']I'] = '@conditional.outer', -- Next if/else end
-            [']L'] = '@loop.outer', -- Next loop end
+            [']M'] = '@function.outer',
+            [']['] = '@class.outer',
+            [']I'] = '@conditional.outer',
+            [']L'] = '@loop.outer',
           },
           goto_previous_start = {
-            ['[m'] = '@function.outer', -- Previous function start
-            ['[['] = '@class.outer', -- Previous class start
-            ['[i'] = '@conditional.outer', -- Previous if/else start
-            ['[l'] = '@loop.outer', -- Previous loop start
+            ['[m'] = '@function.outer',
+            ['[['] = '@class.outer',
+            ['[i'] = '@conditional.outer',
+            ['[l'] = '@loop.outer',
           },
           goto_previous_end = {
-            ['[M'] = '@function.outer', -- Previous function end
-            ['[]'] = '@class.outer', -- Previous class end
-            ['[I'] = '@conditional.outer', -- Previous if/else end
-            ['[L'] = '@loop.outer', -- Previous loop end
+            ['[M'] = '@function.outer',
+            ['[]'] = '@class.outer',
+            ['[I'] = '@conditional.outer',
+            ['[L'] = '@loop.outer',
           },
         },
-        -- You can also enable 'lsp_interop' for better interaction with LSP,
-        -- though this might require 'nvim-lspconfig' setup as well.
-        -- lsp_interop = {
-        --   enable = true,
-        --   peek_definition_if_nil = true,
-        -- },
       },
-      -- === END: nvim-treesitter-textobjects configuration ===
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-
-    -- IMPORTANT: Add nvim-treesitter-textobjects as a dependency here
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
   },
 }
--- vim: ts=2 sts=2 sw=2 et
