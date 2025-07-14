5e36c29c08f1f25e7f110f3c7d6bf3a839dfad07
Sacha Senas <sachasenas@gmail.com>
2025-07-15 00:29:22 +0200

    tmux support

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -46,6 +46,8 @@ require('lazy').setup({
   require 'kickstart.plugins.oil',
 
   require 'kickstart.plugins.toggleterm',
+
+  require 'kickstart.plugins.tmux-navigation',
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
