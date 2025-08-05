0ca42b84a6e711ce552a2bdb6a747dde131d3a20
Sacha Senas <sachasenas@gmail.com>
2025-08-05 21:36:21 +0200

    fix completion plugin for sql

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -4,55 +4,37 @@ return {
     event = 'VimEnter',
     version = '1.*',
     dependencies = {
-      -- Snippet Engine
-      {
-        'L3MON4D3/LuaSnip',
-        version = '2.*',
-        build = (function()
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        opts = {},
-      },
-
+      { 'L3MON4D3/LuaSnip', version = '2.*', build = 'make install_jsregexp', opts = {} },
       'folke/lazydev.nvim',
-
-      -- Dadbod completion
       { 'kristijanhusak/vim-dadbod-completion' },
     },
 
-    --- @module 'blink.cmp'
-    --- @type blink.cmp.Config
     opts = {
       keymap = { preset = 'default' },
-
       appearance = { nerd_font_variant = 'mono' },
-
-      completion = {
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
-      },
+      completion = { documentation = { auto_show = false, auto_show_delay_ms = 500 } },
 
       sources = {
-        -- Default sources
         default = { 'lsp', 'path', 'snippets', 'lazydev', 'dadbod' },
-
         providers = {
-          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          lazydev = {
+            module = 'lazydev.integrations.blink',
+            score_offset = 100,
+          },
           dadbod = {
             name = 'Dadbod',
-            module = 'vim_dadbod_completion.blink', -- required for blink
+            module = 'vim_dadbod_completion.blink',
             score_offset = 75,
-            filetypes = { 'sql', 'mysql', 'plsql' }, -- optional restriction
+            enabled = function()
+              local ft = vim.bo.filetype
+              return ft == 'sql' or ft == 'mysql' or ft == 'plsql'
+            end,
           },
         },
       },
 
       snippets = { preset = 'luasnip' },
-
       fuzzy = { implementation = 'lua' },
-
       signature = { enabled = true },
     },
   },
